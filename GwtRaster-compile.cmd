056f3595ee0b26694f93ba22ada8a28f7d9b3753
Bosco So <geeknight@boscoso.com>
2008-02-15 08:41:05 +0000

    Initial import.

---
+++ GwtRaster-compile.cmd
@@ -0,0 +1,1 @@
+@java -cp "%~dp0\src;%~dp0\bin;C:/gwt/gwt-windows-1.4.60/gwt-user.jar;C:/gwt/gwt-windows-1.4.60/gwt-dev-windows.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\www" %* com.boscomonkey.gwtraster.GwtRaster
---
+++ GwtRaster-shell.cmd
@@ -0,0 +1,1 @@
+@java -cp "%~dp0\src;%~dp0\bin;C:/gwt/gwt-windows-1.4.60/gwt-user.jar;C:/gwt/gwt-windows-1.4.60/gwt-dev-windows.jar" com.google.gwt.dev.GWTShell -out "%~dp0\www" %* com.boscomonkey.gwtraster.GwtRaster/GwtRaster.html
