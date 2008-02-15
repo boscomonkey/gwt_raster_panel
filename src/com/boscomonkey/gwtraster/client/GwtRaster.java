e4b8bad8dfbb2e2c1d56b5164a26b6e7af24fcb1
Bosco So <geeknight@boscoso.com>
2008-02-15 19:29:26 +0000

    - runs on Mac - mouse down starts drawing - mouse up stops drawing - allows clear

--- src/com/boscomonkey/gwtraster/client/GwtRaster.java
+++ src/com/boscomonkey/gwtraster/client/GwtRaster.java
@@ -1,9 +1,16 @@
 package com.boscomonkey.gwtraster.client;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.MouseListener;
 import com.google.gwt.user.client.ui.MouseListenerAdapter;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -13,9 +20,13 @@ import com.google.gwt.user.client.ui.Widget;
  */
 public class GwtRaster implements EntryPoint {
 
+    private HashSet mousedOvers = new HashSet();
+    private Button btnClear = new Button("Clear");
+
     protected class Dot extends HTML {
-        private static final int HEIGHT = 10;
-        private static final int WIDTH = 10;
+        public static final String CSS_MOUSEOVER = "cbg-GwtRaster-mouseover";
+        private static final int HEIGHT = 4;
+        private static final int WIDTH = 4;
 
         public int x;
         public int y;
@@ -33,15 +44,27 @@ public class GwtRaster implements EntryPoint {
         }
     }
 
-    protected MouseListenerAdapter listener = new MouseListenerAdapter() {
-        private static final String CSS_MOUSEOVER = "cbg-GwtRaster-mouseover";
-
+    private boolean bMouseDown = false;
+    protected MouseListenerAdapter moveListener = new MouseListenerAdapter() {
         public void onMouseEnter(Widget sender) {
-            sender.addStyleName(CSS_MOUSEOVER);
+            if (bMouseDown) {
+                sender.addStyleName(Dot.CSS_MOUSEOVER);
+                mousedOvers.add(sender);
+            }
         }
-
-        public void onMouseLeave(Widget sender) {
-            sender.removeStyleName(CSS_MOUSEOVER);
+    };
+    private MouseListenerAdapter mouseBtnListener = new MouseListenerAdapter() {
+        public void onMouseDown(Widget sender, int x, int y) {
+            if (!bMouseDown) {
+                DOM.setCapture(sender.getElement());
+                bMouseDown = true;
+            }
+        }
+        public void onMouseUp(Widget sender, int x, int y) {
+            if (bMouseDown) {
+                DOM.releaseCapture(sender.getElement());
+                bMouseDown = false;
+            }
         }
     };
 
@@ -49,18 +72,34 @@ public class GwtRaster implements EntryPoint {
      * This is the entry point method.
      */
     public void onModuleLoad() {
+        FocusPanel focus = new FocusPanel();
+        focus.addMouseListener(mouseBtnListener);
+
         FlowPanel panel = new FlowPanel();
+        focus.setWidget(panel);
         panel.setStyleName("cbg-GwtRaster");
 
-        for (int x = 0; x < 40; x++) {
-            for (int y = 0; y < 30; y++) {
+        for (int x = 0; x < 100; x++) {
+            for (int y = 0; y < 75; y++) {
                 HTML dot = new Dot(x, y);
-                dot.addMouseListener(listener);
+                dot.addMouseListener(moveListener);
                 panel.add(dot);
             }
         }
 
-        RootPanel.get("slot1").add(panel);
+        RootPanel.get("slot1").add(focus);
+
+        // set up "Clear" button
+        btnClear.addClickListener(new ClickListener() {
+            public void onClick(Widget sender) {
+                for (Iterator iter = mousedOvers.iterator(); iter.hasNext();) {
+                    Widget w = (Widget) iter.next();
+                    w.removeStyleName(Dot.CSS_MOUSEOVER);
+                }
+                mousedOvers.clear();
+            }
+        });
+        RootPanel.get("slot2").add(btnClear);
     }
 
 }
