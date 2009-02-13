a626a0ca0524e4cd912a682a23db5370bea72897
Bosco So <git@boscoso.com>
2009-02-13 11:20:37 -0800

    exported getDots() method to PanelWidget so lit dots can be accessed from native JavaScript

--- src/com/boscomonkey/gwtraster/client/PanelWidget.java
+++ src/com/boscomonkey/gwtraster/client/PanelWidget.java
@@ -4,27 +4,55 @@ import org.timepedia.exporter.client.Export;
 import org.timepedia.exporter.client.ExportPackage;
 import org.timepedia.exporter.client.Exportable;
 
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArray;
 import com.google.gwt.user.client.ui.RootPanel;
 
 @ExportPackage("gwtraster")
 public class PanelWidget extends RasterPanel implements Exportable {
+
+    /** JavaScript overlay type that represents {x:1, y:2} */
+    public static class Dot extends JavaScriptObject {
+        protected Dot() {}
+        
+        public final native int x() /*-{ return this.x; }-*/;
+        public final native int y() /*-{ return this.y; }-*/;
+    }
+
     @Export
     public PanelWidget() { super(); }
+
+    /**
+     * Adds this RasterPanel as a child to the DOM element identified by
+     * "id"
+     * 
+     * @param id
+     */
+    @Export
+    public void addTo(String id) {
+        RootPanel.get(id).add(this);
+    }
 
     @Export
     @Override
     public void clear() {
         super.clear();
     }
-
+    
     /**
-     * Appends this RasterPanel as a child of the DOM element identified by
-     * "id"
-     * 
-     * @param id
+     * @return native JavaScript array of Dot JSON objects - {x:..., y:...}
      */
     @Export
-    public void appendAsChild(String id) {
-        RootPanel.get(id).add(this);
+    public JsArray<Dot> getDots() {
+        XyCoord[] coords = getCoords();
+        JsArray<Dot> dots = newArray();
+        for (int i = 0; i < coords.length; i++)
+            dots.set(i, newDot(coords[i].x(), coords[i].y()));
+
+        return dots;
     }
+    
+    private final native JsArray<Dot> newArray() /*-{ return []; }-*/;
+    
+    private final native Dot newDot(int i, int j) /*-{ return {x:i, y:j}; }-*/;
 }
